f0e364f7acc614129096e24c833d5cc936f9ef99
WEOM <142773535+asianweom@users.noreply.github.com>
2023-08-30 15:12:50 +0800

    Update faculties.js

--- scripts/faculties.js
+++ scripts/faculties.js
@@ -224,7 +224,7 @@ const workshopParticipantData = [
 ];
 
 // Add faculty cards to respective sections
-addFacultyCards("keynote", keynoteData);
+//addFacultyCards("keynote", keynoteData);
 addFacultyCards("organizing-faculty", organizingFacultyData);
 addFacultyCards("workshop-faculty", workshopFacultyData);
 //addFacultyCards("workshop-participants", workshopParticipantData);
